f8a7803d44f0e2da8aface3d42b696fb47cb04f7
nicole.young <nicoledyoung94@gmail.com>
2021-04-23 01:25:49 -0400

    fe style: adds style to products page

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -10,28 +10,54 @@ export class AllProducts extends React.Component {
 
   render() {
     const { products } = this.props;
-    console.log('products:', products);
+
     return (
-      <div>
-        {products.length ? (
-          products.map((product) => (
-            <div key={product.id}>
-              <img src={product.imageUrl} alt={product.name} />
-              <Link to={`/products/${product.id}`}>
-                <h4>{product.name}</h4>
-              </Link>
-              <p>${product.price / 100}</p>
+      <div className="container">
+        <img
+          src="https://www.thespruceeats.com/thmb/E-nBmWUIiYvM7hVBuYoOuLE6yIY=/2065x1162/smart/filters:no_upscale()/GettyImages-1043955542-5c82cf8846e0fb000143197b.jpg"
+          alt="grace sipper cocktails"
+          className="rounded img-fluid"
+        />
+        <hr />
+
+        <figure className="text-center">
+          <blockquote className="blockquote">
+            <h1 className="display-6">
+              Artfully crafted cocktails, from the comfort of your own home.
+            </h1>
+          </blockquote>
+          <figcaption className="text-muted">Ship, Sip, Enjoy.</figcaption>
+        </figure>
+        <hr />
+        <div className="card-group">
+          {products.length ? (
+            products.map((product) => (
+              <div key={product.id} className="col-sm-12 col-md-6 col-lg-4">
+                <Link to={`/products/${product.id}`}>
+                  <div className="card bg-light text-white ">
+                    <img
+                      src={product.imageUrl}
+                      alt={product.name}
+                      className="card-img"
+                    />
+                    <div className="card-img-overlay">
+                      <h4 className="card-title">{product.name}</h4>
+                      <p className="card-text">${product.price / 100}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div>
+              <h3 className="h3 text-align-center">Mixing it up...</h3>
+              <img
+                src="https://media4.giphy.com/media/h8fnSteH0zUrlMf291/giphy.gif"
+                alt="loading"
+              />
             </div>
-          ))
-        ) : (
-          <div>
-            <h3>Loading...</h3>
-            <img
-              src="https://media1.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
-              alt="loading"
-            />
-          </div>
-        )}
+          )}
+        </div>
       </div>
     );
   }
