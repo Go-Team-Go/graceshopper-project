a6afafc4be9cf69e0f00326f70aaf5b5e23c37e2
nicole.young <nicoledyoung94@gmail.com>
2021-04-23 01:24:34 -0400

    fe refact: exact logged in all path

--- client/routes.js
+++ client/routes.js
@@ -23,8 +23,8 @@ class Routes extends Component {
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-            <Route path="/products" component={AllProducts} />
+            <Redirect path="/products" component={AllProducts} />
+            <Route exact path="/products" component={AllProducts} />
             <Route path="/products/:id" component={SingleProduct} />
           </Switch>
         ) : (
