4ed9789f25f8dc69f2fc6a342eef568561028373
gg447062 <gg447062@gmail.com>
2021-04-24 14:30:14 -0500

    BE refact: updates variable names in put route

--- server/api/cart.js
+++ server/api/cart.js
@@ -60,11 +60,10 @@ router.post('/', async (req, res, next) => {
 router.put('/', async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
-    const { quantity, productId } = req.body;
+    const { newQuantity, productId } = req.body;
     const item = await Cart.findOne({
       where: { userId: user.id, productId },
     });
-    const newQuantity = item.quantity + quantity;
     const updatedItem = await item.update({ quantity: newQuantity });
     res.send(updatedItem);
   } catch (err) {
