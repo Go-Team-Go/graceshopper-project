dfe53951b07c02efb36b9175183b5834b4306505
gg447062 <gg447062@gmail.com>
2021-04-24 13:42:56 -0500

    BE feat: adds delete and history api routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  models: { Cart, Product, User },
+  models: { Cart, User },
 } = require('../db');
 module.exports = router;
 
@@ -8,7 +8,20 @@ router.get('/', async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
     const items = await Cart.findAll({
-      where: { userId: user.id },
+      where: { userId: user.id, purchased: false },
+    });
+
+    res.send(items);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/history', async (req, res, next) => {
+  try {
+    const user = await User.findByToken(req.headers.authorization);
+    const items = await Cart.findAll({
+      where: { userId: user.id, purchased: true },
     });
 
     res.send(items);
@@ -18,19 +31,27 @@ router.get('/', async (req, res, next) => {
 });
 
 //POST /api/cart/
-
 router.post('/', async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
     const { quantity, productId, name, price } = req.body;
-    const item = await Cart.create({
-      userId: user.id,
-      quantity,
-      productId,
-      name,
-      price,
+    const [item, created] = await Cart.findOrCreate({
+      where: {
+        userId: user.id,
+        productId,
+        name,
+        price,
+      },
     });
-    res.send(item);
+    if (created) {
+      item.quantity = quantity;
+      await item.save();
+      res.send(item);
+    } else {
+      const newQuantity = item.quantity + quantity;
+      const updatedItem = await item.update({ quantity: newQuantity });
+      res.send(updatedItem);
+    }
   } catch (err) {
     next(err);
   }
@@ -48,5 +69,19 @@ router.put('/', async (req, res, next) => {
     res.send(updatedItem);
   } catch (err) {
     next(err);
+  }
+});
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const user = await User.findByToken(req.headers.authorization);
+    const { productId } = req.params;
+    const itemToDelete = await Cart.findOne({
+      where: { userId: user.id, productId },
+    });
+    await itemToDelete.destroy();
+    res.send(itemToDelete);
+  } catch (error) {
+    next(error);
   }
 });
