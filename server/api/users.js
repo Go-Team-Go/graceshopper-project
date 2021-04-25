304e529d262f9058bc6bc77d6fb2bd703b592c27
shorefb <reid.vanwagner@gmail.com>
2021-04-25 14:05:11 -0400

    one other commit

--- server/api/users.js
+++ server/api/users.js
@@ -2,27 +2,13 @@ const router = require('express').Router();
 const {
   models: { User },
 } = require('../db');
-<<<<<<< HEAD
-module.exports = router;
-=======
->>>>>>> main
 const { requireToken, isAdmin } = require('./gateKeeperMiddleWare');
 
 module.exports = router;
 
-//GET api/users    ----> find all logged in admin users
+//GET api/users    ----> find all logged in admin users new stuff
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
-<<<<<<< HEAD
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username'],
-    });
-    res.json(users);
-=======
->>>>>>> main
     const allUsers = await User.findAll({
       attributes: ['id', 'username', 'email', 'admin'],
     });
