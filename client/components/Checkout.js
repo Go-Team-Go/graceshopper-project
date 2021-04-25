9d5d6d2b62e015de2a2782eefce3e7d5f23f3941
nicole.young <nicoledyoung94@gmail.com>
2021-04-25 19:10:58 -0400

    fe style: adds styles to checkout page

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -4,8 +4,14 @@ import { connect } from 'react-redux';
 export default class Checkout extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Thank you for your order!</h2>
+      <div className="container justify-content-center">
+        <h2 className="display-4 text-center">Thank you for your order!</h2>
+        <hr />
+        <img
+          src="https://www.thespruceeats.com/thmb/E-nBmWUIiYvM7hVBuYoOuLE6yIY=/2065x1162/smart/filters:no_upscale()/GettyImages-1043955542-5c82cf8846e0fb000143197b.jpg"
+          alt="grace sipper cocktails"
+          className="rounded img-fluid"
+        />
       </div>
     );
   }
