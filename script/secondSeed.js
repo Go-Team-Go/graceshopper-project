0ea1d6bd3ad9306c601a9cc1665ad01d0e24c90c
nicole.young <nicoledyoung94@gmail.com>
2021-04-25 10:21:32 -0400

    seed wip

--- script/secondSeed.js
+++ script/secondSeed.js
@@ -81,8 +81,33 @@ const products = [
   },
   //   ...new stuff
   {
-    name: '',
+    name: 'grid',
     description: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/esfasy1n/resolutionhighball_serveonly_orangeblossom.jpg?width=475&height=900&mode=crop',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: '',
+    description:
+      'https://media.seedlipdrinks.com/PR1802/media/0fqlt0q5/resolutionhighball_serveonly_coldbrew.jpg?width=475&height=900&mode=crop',
+    imageUrl: '',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: '',
+    description:
+      'https://media.seedlipdrinks.com/PR1802/media/awelyj4h/resolutionhighball_serveonly_booch.jpg?width=475&height=900&mode=crop',
+    imageUrl: '',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: 'Node Nog',
+    description:
+      'https://media.seedlipdrinks.com/PR1802/media/v4upvgvb/alternative-egg-nog.png?width=475&height=900&mode=crop',
     imageUrl: '',
     price: 1000,
     quantity: 10,
@@ -90,6 +115,31 @@ const products = [
   {
     name: '',
     description: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/qz3gc0wb/apple-highball-updated.png?width=475&height=900&mode=crop',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: '',
+    description: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/0p4porsx/grove-ginger-highball.png?width=475&height=900&mode=crop',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: '',
+    description: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/ydglihha/eastside.png?width=475&height=900&mode=crop',
+    price: 1000,
+    quantity: 10,
+  },
+  {
+    name: '',
+    description:
+      'https://media.seedlipdrinks.com/PR1802/media/ng0pc3h4/minosa-2.png?width=475&height=900&mode=crop',
     imageUrl: '',
     price: 1000,
     quantity: 10,
@@ -97,238 +147,224 @@ const products = [
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/xwojod5i/citrus-fizz.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/duvnek5c/spice-and-black-2.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/o2yplmxu/damascus.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/gx0mwh1a/spiced-mule.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/tscfvwwr/toddy.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/2bvllgvx/from-the-field.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/gl3dmbm3/husk.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/a4qiet3d/laxtons-gage.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/rktl2iix/misowari.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/pykhbzq0/mint-cooler.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/ihmkisib/watermelon-sour.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/r2yjcycc/fireworks.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/owaccgmb/rhubarb-spritz.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/3sonnf51/the-bees-peas.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/mfwlq1bn/pear-parsnip.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/gd1pyhux/garden-sour.jpg?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/lb2jfwxf/cherry-blossom.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://media.seedlipdrinks.com/PR1802/media/cbxmjzjg/hedge-your-bets.png?width=475&height=900&mode=crop',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://images.ctfassets.net/b0q5etab7zkl/60S91HzDshhYQJNlRU7VnQ/53b40417098415c2a5768456860b74a2/Seedlip_Savoy_S1_w1b__1_.jpg',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_UbajuCq_y8YASUk-I8vyNODe6MrfZO9mNw&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      ' https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe0W5AFjWGsxT4prkUXje7MJjNmWSQdvZ7sQ&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuXmm3IfR-y31cMWvgTY4sYbZenUKpcl1TXA&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeWmPMtao3PALW1PvuMXkAVo58ZQlMyiTUvA&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeHnzbFa63UhKDY4KjKDiLY4bFI0Jd6tiv5g&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStAwCzBMC1RkdMqQf7gAm-UmfEhFI5vewMpg&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZhXGxXL9BBosp_B62CTO-L9KfwVep3EYL8g&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlZzeFs1SzvUeAuJOJLepPU5ZkUKCqkwLMbImJULTjhcaRVou9PJOL3mtHv-KvmlM4s5c&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
   {
     name: '',
     description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
-    price: 1000,
-    quantity: 10,
-  },
-  {
-    name: '',
-    description: '',
-    imageUrl: '',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSekwgv_gaWz4v914gBgIDxUZnhQTq6N8ALqA&usqp=CAU',
     price: 1000,
     quantity: 10,
   },
